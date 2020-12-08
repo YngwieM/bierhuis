c2e09317e774f7a4a53c00c2411be12b2b482918
Yngwie Mariman <yngwie.mariman@vdabcampus.be>
2020-12-08 09:21:05 +0100

    Test bierhuis 8/12

---
+++ src/main/java/be/vdab/bierhuis/queryresults/MandjeLijst.java
@@ -0,0 +1,33 @@
+package be.vdab.bierhuis.queryresults;
+
+import java.math.BigDecimal;
+
+public class MandjeLijst {
+
+    private final String naam;
+    private final BigDecimal prijs;
+    private final long aantal;
+
+    public MandjeLijst(String naam, BigDecimal prijs, long aantal) {
+        this.naam = naam;
+        this.prijs = prijs;
+        this.aantal = aantal;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public BigDecimal getPrijs() {
+        return prijs;
+    }
+
+    public long getAantal() {
+        return aantal;
+    }
+
+    public BigDecimal getTotaal() {
+        BigDecimal aantalBig = new BigDecimal(aantal);
+        return prijs.multiply(aantalBig);
+    }
+}
--- src/main/java/be/vdab/bierhuis/services/DefaultBierService.java
+++ src/main/java/be/vdab/bierhuis/services/DefaultBierService.java
@@ -1,10 +1,13 @@
 package be.vdab.bierhuis.services;
 
 import be.vdab.bierhuis.domain.Bier;
+import be.vdab.bierhuis.queryresults.MandjeLijst;
 import be.vdab.bierhuis.repositories.BierRepository;
+import be.vdab.bierhuis.sessions.Mandje;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -13,9 +16,11 @@ import java.util.Set;
 @Transactional
  class DefaultBierService implements BierService {
     private final BierRepository bierRepository;
+    private final Mandje mandje;
 
-    DefaultBierService(BierRepository bierRepository) {
+    DefaultBierService(BierRepository bierRepository, Mandje mandje) {
         this.bierRepository = bierRepository;
+        this.mandje = mandje;
     }
 
     @Override
@@ -37,4 +42,11 @@ import java.util.Set;
     public List<Bier> findByIds(Set<Long> ids) {
         return bierRepository.findByIds(ids);
     }
+
+//    public List<MandjeLijst> toonBestelLijn() {
+//        List<MandjeLijst> mandjelijst = new ArrayList<>();
+//
+//        var bestellijnIds = mandje.getIds();
+//        for ()
+//    }
 }
